d8f1e9c82d2c051ab4e09e69051d17b6b88bbd76
Игорь Камраков <i.kamrakov@gmail.com>
2018-06-06 23:14:24 +0300

    add slider

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 app.set('port', process.env.PORT || 3000);
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/View/index.html');
+
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
 var server = app.listen(app.get('port'), function(){
