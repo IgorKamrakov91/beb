177cc180168baa4afc5a956741e1bfc8c487412b
Игорь Камраков <i.kamrakov@gmail.com>
2018-06-06 21:29:02 +0300

    port

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/View/index.html');
 });
 
-app.listen(3000);
-
-console.log("Running at Port 3000");
+var server = app.listen(app.get('port'), function(){
+  console.log('Listening on port' + app.get('port'))
+});
